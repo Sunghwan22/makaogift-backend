488598eaba176af2d55692cbefdb003fc4fd5215
sunghwan22 <tidls3144@naver.com>
2022-10-02 22:30:33 +0900

    스프링  프로젝트 생성

---
+++ src/main/java/megaptera/makaoGift/MakaoGiftApplication.java
@@ -0,0 +1,13 @@
+package megaptera.makaoGift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakaoGiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakaoGiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/megaptera/makaoGift/MakaoGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package megaptera.makaoGift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakaoGiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
